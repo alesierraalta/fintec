464844dc2bb81211be8e69c37744ecb87206f7a6
FinTec Developer <dev@fintec.local>
2026-05-01 10:53:36 -0400

    chore(repo): remove obsolete generated artifacts

--- run_emulator.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Android Emulator (Pixel 7)...
-set ANDROID_HOME=C:\Users\ismar\AppData\Local\Android\Sdk
-"C:\Users\ismar\AppData\Local\Android\Sdk\emulator\emulator.exe" -avd Pixel_7_API_36
-pause
